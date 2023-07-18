2daebccf4094dc062b4157d96813c76ac1f6a153
dnjsxoghd <feelchok1234@gmail.com>
2023-07-19 00:22:30 +0900

    ud 19

--- ReactUd/src/components/ExpenseItem.js
+++ ReactUd/src/components/ExpenseItem.js
@@ -1,11 +1,18 @@
+import './ExpenseItem.css'
+
 
 function ExpenseItem() {
+
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance'
+    const expenseAmount = 294.67
+
     return(
-    <div>
-    <div>March 28th 2021</div>
-    <div>
-        <h2>Car Insurance</h2>
-    <div>$294.67</div>
+    <div className="expense-item">
+    <div>{expenseDate.toISOString()}</div>
+    <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+    <div className='expense-item__price'>${expenseAmount}</div>
     </div>
     </div>) // 가로가 없어도 된다. 가독성을 위해서 만든 것이다.
 }
