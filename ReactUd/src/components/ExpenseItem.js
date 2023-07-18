b41161c4742c3571c4b65df1bfe4a5616486da6d
dnjsxoghd <feelchok1234@gmail.com>
2023-07-18 23:56:25 +0900

    ReactUD

---
+++ ReactUd/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+
+function ExpenseItem() {
+    return(
+    <div>
+    <div>March 28th 2021</div>
+    <div>
+        <h2>Car Insurance</h2>
+    <div>$294.67</div>
+    </div>
+    </div>) // 가로가 없어도 된다. 가독성을 위해서 만든 것이다.
+}
+
+export default ExpenseItem;
