13cf9dec19770585fba9e49273bca1a93135221b
dnjsxoghd <feelchok1234@gmail.com>
2023-11-21 17:21:38 +0900

    component props Object destur

--- ReactUdemy/ReactCode/React-2-ReactBasic/01-starting-setup/src/components/ExpenseItem.js
+++ ReactUdemy/ReactCode/React-2-ReactBasic/01-starting-setup/src/components/ExpenseItem.js
@@ -1,19 +1,19 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+function ExpenseItem({date, title, amount}) {
 
     // const expenseDate = new Date(2021, 2, 28);
     // const expenseTitle = 'Car Insurance'
     // const expenseAmount = 294.67
-    console.log(props);
+    console.log(date);
 
 
   return(
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
