c57b07b5040196f497a9f4d3012d1328de24fac4
dnjsxoghd <feelchok1234@gmail.com>
2023-11-22 15:55:33 +0900

    React Event and EventHandler HTML proprty

---
+++ ReactUdemy/ReactCode/React-5-StateAndEvent/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,23 @@
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+
+function ExpenseItem({date, title,amount}) {
+
+  const clickHandler = ()=>{
+    console.log('Clicked Me')
+  }
+
+  return(
+    <Card className="expense-item">
+      <ExpenseDate date={date}></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
