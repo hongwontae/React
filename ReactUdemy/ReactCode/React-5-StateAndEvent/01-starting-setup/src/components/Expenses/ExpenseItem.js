ae3155eb17eb1d33d04ba861b39f06a95d470650
dnjsxoghd <feelchok1234@gmail.com>
2023-11-22 16:57:42 +0900

    useState

--- ReactUdemy/ReactCode/React-5-StateAndEvent/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ ReactUdemy/ReactCode/React-5-StateAndEvent/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,14 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+import {useState} from 'react';
 
 function ExpenseItem({date, title,amount}) {
 
+  const [title1, setTitle1] = useState(title);  
+
   const clickHandler = ()=>{
+    setTitle1('Updated');
     console.log('Clicked Me')
   }
 
@@ -12,7 +16,7 @@ function ExpenseItem({date, title,amount}) {
     <Card className="expense-item">
       <ExpenseDate date={date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{title1}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
