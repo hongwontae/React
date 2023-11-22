7d6f49ab418fdda4c003bee1c0027acb47456df3
dnjsxoghd <feelchok1234@gmail.com>
2023-11-22 17:55:21 +0900

    input.value access

--- ReactUdemy/ReactCode/React-5-StateAndEvent/01-starting-setup/src/App.js
+++ ReactUdemy/ReactCode/React-5-StateAndEvent/01-starting-setup/src/App.js
@@ -1,5 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
 import React from 'react';
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -33,7 +34,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense></NewExpense>
       <Expenses item={expenses}></Expenses>
     </div>
   );
