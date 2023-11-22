1b5fe7aa2804c256041bc074721acc04ada4d250
dnjsxoghd <feelchok1234@gmail.com>
2023-11-22 17:14:04 +0900

    React useState High concept

--- ReactUdemy/ReactCode/React-5-StateAndEvent/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ ReactUdemy/ReactCode/React-5-StateAndEvent/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -3,8 +3,9 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import {useState} from 'react';
 
-function ExpenseItem({date, title,amount}) {
+function ExpenseItem({date, title, amount}) {
 
+  console.log(useState(title)); // useState의 반환값은 배열이다.
   const [title1, setTitle1] = useState(title);  
 
   const clickHandler = ()=>{
