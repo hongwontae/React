db1bc8c727c8d0ebbb35423f23f94ea84a50876d
dnjsxoghd <feelchok1234@gmail.com>
2023-11-28 16:09:55 +0900

    List workigj

--- ReactUdemy/ReactCode/React-6-RenderingAndConditionalCotent/01-starting-setup/src/components/Expenses/Expenses.js
+++ ReactUdemy/ReactCode/React-6-RenderingAndConditionalCotent/01-starting-setup/src/components/Expenses/Expenses.js
@@ -5,24 +5,33 @@ import ExpenseFilter from "./ExpenseFilter";
 import { useState } from "react";
 
 function Expenses(props) {
+  const [filterYear, setFilterYear] = useState("2020");
 
-
-  const [filterYear, setFilterYear] = useState('2020');
-
-  const filterChangeHandler = (selectedYear)=>{
+  const filterChangeHandler = (selectedYear) => {
     setFilterYear(selectedYear);
-  }
+  };
 
+  const filteredExpenses = props.item.filter(expense => {
+    return expense.date.getFullYear().toString() === filterYear;
+  })
 
   return (
     <div>
       <Card className="expenses">
-        <ExpenseFilter selected={filterYear} onChangeFilter={filterChangeHandler} />
-      {
-        props.item.map((expense)=>{
-         return <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>
-        })
-      }
+        <ExpenseFilter
+          selected={filterYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {filteredExpenses.map((expense) => {
+          return (
+            <ExpenseItem
+            key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          );
+        })}
       </Card>
     </div>
   );
