532620df1c120be4967b8b7fc37a922b8afb000c
dnjsxoghd <feelchok1234@gmail.com>
2023-12-06 00:09:26 +0900

    reveiw-All-1

--- ReactUdemy/ReactCode/testReact/testreact/src/App.js
+++ ReactUdemy/ReactCode/testReact/testreact/src/App.js
@@ -4,10 +4,20 @@ function App() {
   
 
   const test = [1,2,3]
+  const info = [{
+    name : 'hwt',
+    age : 1000
+  },{
+    name : 'hen',
+    age : 32
+  },{
+    name : 'cou',
+    age : 2000
+  }]
 
   return (
     <div className="App">
-      <Test test={test}></Test>
+      <Test test={test} infoProps={info}></Test>
     </div>
   );
 }
--- ReactUdemy/ReactCode/testReact/testreact/src/Components/Test.js
+++ ReactUdemy/ReactCode/testReact/testreact/src/Components/Test.js
@@ -3,7 +3,7 @@ import Segment from "./Segment";
 function Test(props){
     return(
         <div>
-            <Segment good={props.test[0]} good2={props.test[1]} good3={props.test[2]}></Segment>
+            <Segment information={props.infoProps}></Segment>
         </div>
     )
 }
