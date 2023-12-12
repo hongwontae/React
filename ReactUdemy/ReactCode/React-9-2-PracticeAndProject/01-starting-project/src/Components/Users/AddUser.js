b9c623c36282550170a36b559985beb93ee24914
dnjsxoghd <feelchok1234@gmail.com>
2023-12-12 15:24:03 +0900

    test

--- ReactUdemy/ReactCode/React-9-2-PracticeAndProject/01-starting-project/src/Components/Users/AddUser.js
+++ ReactUdemy/ReactCode/React-9-2-PracticeAndProject/01-starting-project/src/Components/Users/AddUser.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css'
+import Button from '../UI/Button';
 
 const AddUser = (props)=>{
+
+    
 
     const addUserHandler = (event)=>{
         event.preventDefault();
@@ -16,7 +19,7 @@ const AddUser = (props)=>{
             <input id='username' type='text'></input>
             <label htmlFor='age'>Age(Years)</label>
             <input id='age' type='number'></input>
-            <button type='submit'>Add User</button>
+            <Button type="submit">Add User</Button>
         </form>
         </Card>
     )
