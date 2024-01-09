f1b98beaa8df32457d0cf9fc0b83d18f595bb5b4
dnjsxoghd <feelchok1234@gmail.com>
2024-01-09 22:23:48 +0900

    useHooks

---
+++ ReactUdemy/ReactCode2/React-8-RefsAndPortals/01-starting-project/src/components/TimerChallenge.jsx
@@ -0,0 +1,49 @@
+import { useState, useRef } from "react";
+import ResultModal from "./ResultModal";
+
+const TimerChallenge = ({ title, targetTime }) => {
+  const timer = useRef();
+  const dialog = useRef();
+
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+      dialog.current.showModal();
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
+  return (
+    <>
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        result={"lost"}
+      ></ResultModal>
+
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} seconde{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? "Stop" : "Start"} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? "active" : undefined}>
+          {timerStarted ? "Time is Running" : "Timer inactive"}
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default TimerChallenge;
