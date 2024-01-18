19d4e1d46c48d6d158a1f5eb8996d0895aba61f2
dnjsxoghd <feelchok1234@gmail.com>
2024-01-18 21:46:59 +0900

    memo useCallback useMemo

--- ReactUdemy/ReactCode2/React-12-Pratice-Project3/01-starting-project/src/components/Summary.jsx
+++ ReactUdemy/ReactCode2/React-12-Pratice-Project3/01-starting-project/src/components/Summary.jsx
@@ -2,21 +2,40 @@ import quizCompleteImg from "../assets/quiz-complete.png";
 import QUESTIONS from "../question.js";
 
 const Summary = ({ userAnswers }) => {
+
+    // const skippedAnswer = userAnswers.filter((ele, idx, arr)=>{
+    //     return ele === null
+    // });
+    
+    // const correctAnswer = userAnswers.filter((ele, idx, arr) => {
+    //     return ele === QUESTIONS[idx].answers[0]
+    // })
+
+    // const skippedAnswerShare = Math.round(
+    //     (skippedAnswer.length/ userAnswers.length) * 100
+    // )
+    // const correctAnswerShare = Math.round(
+    //     (correctAnswer.length / userAnswers.length) * 100
+    // )
+
+    // const wrongAnswerShare = 100 - skippedAnswerShare - correctAnswerShare;
+
+
   return (
     <div id="summary">
       <img src={quizCompleteImg}></img>
       <h2>Quiz Completed!</h2>
       <div id="summary-stats">
         <p>
-          <span className="number">10%</span>
+          <span className="number">{}%</span>
           <span className="text">skipped</span>
         </p>
         <p>
-          <span className="number">10%</span>
+          <span className="number">{}%</span>
           <span className="text">answered correctly</span>
         </p> 
         <p>
-          <span className="number">10%</span>
+          <span className="number">{}%</span>
           <span className="text">answered incorrectly</span>
         </p>
       </div>
@@ -34,7 +53,7 @@ const Summary = ({ userAnswers }) => {
             }
 
           return (
-            <li>
+            <li key={idx}>
               <h3>{idx+1}</h3>
               <p className="question">{QUESTIONS[idx].text}</p>
               <p className={cssClass}>{ele ? ele : 'Skipped'}</p>
