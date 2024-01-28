3e3280076179016cfb249bae7e78768179d874eb
dnjsxoghd <feelchok1234@gmail.com>
2024-01-28 23:43:20 +0900

    Input State Management

--- ReactUdemy/ReactCode2/React-17-Form-Input-FormData/01-starting-project/src/components/Login.jsx
+++ ReactUdemy/ReactCode2/React-17-Form-Input-FormData/01-starting-project/src/components/Login.jsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
+
 export default function Login() {
+  // const [enterEmail, setEnterEmail] = useState("");
+  // const [enterPassword, setEnterPassword] = useState("");
+
+  const [enterValue, setEnterValue] = useState({
+    email : '',
+    password : ''
+  })
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log('submiitted')
+    console.log(enterValue)
   }
 
+  // function handleEmail(e) {
+  //   setEnterEmail(e.target.value);
+  // }
+
+  // function handlePassword(e) {
+  //   setEnterPassword(e.target.value);
+  // }
+
+  function handleFormData(identifier, value) {
+    setEnterValue(prevState => {
+      return {
+        ...prevState,
+        [identifier] : value
+      }
+    })
+  }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -13,12 +38,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event)=>handleFormData('email', event.target.value)}
+            value={enterValue.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event)=>handleFormData('password', event.target.value)}
+            value={enterValue.password}
+          />
         </div>
       </div>
 
