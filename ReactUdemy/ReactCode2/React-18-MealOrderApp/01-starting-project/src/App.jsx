8b8c5eae1687a620f958025bf64fb31566c7c8b6
dnjsxoghd <feelchok1234@gmail.com>
2024-01-30 16:08:47 +0900

    formatter

--- ReactUdemy/ReactCode2/React-18-MealOrderApp/01-starting-project/src/App.jsx
+++ ReactUdemy/ReactCode2/React-18-MealOrderApp/01-starting-project/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Meal from "./components/Meal";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Meal></Meal>
     </>
   );
 }
