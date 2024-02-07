a9903ef87328f2fa02970725550ad431a1e91579
dnjsxoghd <feelchok1234@gmail.com>
2024-02-07 23:06:37 +0900

    loader-2

--- ReactUdemy/ReactCode2/React-21-RouterSPA/12-adv-starting-project/frontend/src/pages/Events.js
+++ ReactUdemy/ReactCode2/React-21-RouterSPA/12-adv-starting-project/frontend/src/pages/Events.js
@@ -1,13 +1,24 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData } from "react-router-dom";
 
-import EventsList from '../components/EventsList';
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
-
   const data = useLoaderData();
+  const events = data.events;
+  // console.log(data);
 
-
-  return <EventsList events={data}></EventsList>
+  return <EventsList events={events}></EventsList>;
 }
 
 export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+  console.log(response)
+  if (!response.ok) {
+    //...
+  } else {
+    const resData = await response.json();
+    return resData
+  }
+}
