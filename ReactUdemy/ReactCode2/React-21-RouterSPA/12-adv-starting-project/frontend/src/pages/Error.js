e9c2861a2a44f8b941ae5ce4551a6e8e5a94abe6
dnjsxoghd <feelchok1234@gmail.com>
2024-02-09 00:28:54 +0900

    router

--- ReactUdemy/ReactCode2/React-21-RouterSPA/12-adv-starting-project/frontend/src/pages/Error.js
+++ ReactUdemy/ReactCode2/React-21-RouterSPA/12-adv-starting-project/frontend/src/pages/Error.js
@@ -1,28 +1,31 @@
-import { useRouteError } from "react-router-dom";
-import PageContent from "../components/PageContent";
+import { useRouteError } from 'react-router-dom';
+import MainNavigation from '../components/MainNavigation';
+
+import PageContent from '../components/PageContent';
 
 function ErrorPage() {
+  const error = useRouteError();
 
-    const error = useRouteError();
-    console.log('error useRouteError')
-    console.log(error)
+  let title = 'An error occurred!';
+  let message = 'Something went wrong!';
 
-    let title = 'An error occurred!'
-    let message = 'Something went wrong!'
+  if (error.status === 500) {
+    message = error.data.message;
+  }
 
-    if(error.status ===500){
-        message = JSON.parse(error.data).message
-    }
+  if (error.status === 404) {
+    title = 'Not found!';
+    message = 'Could not find resource or page.';
+  }
 
-    if(error.status === 404){
-        title = 'Not Found!'
-        message = 'Could not find resource or page'
-    }
-    
-
-  return <PageContent title={title}>
-    <p>{message}</p>
-  </PageContent>;
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 }
 
 export default ErrorPage;
--- ReactUdemy/ReactCode2/React-21-RouterSPA/12-adv-starting-project/frontend/src/pages/Events.js
+++ ReactUdemy/ReactCode2/React-21-RouterSPA/12-adv-starting-project/frontend/src/pages/Events.js
@@ -1,38 +1,35 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, json } from 'react-router-dom';
 
-import EventsList from "../components/EventsList";
+import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const events = useLoaderData();
-  const data = events.events
-  console.log('events variable')
-  console.log(events)
+  const data = useLoaderData();
 
-  console.log('data variable')
-  console.log(data)
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+  const events = data.events;
 
-
-  if (events.isError) {
-    return <p>{events.message}</p>;
-  }
-
-  return <EventsList events={data}></EventsList>;
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/events");
+  const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
-    throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
-      status: 500,
-    });
+    // return { isError: true, message: 'Could not fetch events.' };
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+    //   status: 500,
+    // });
+    throw json(
+      { message: 'Could not fetch events.' },
+      {
+        status: 500,
+      }
+    );
   } else {
-    console.log('else')
-    console.log(response);
-    
-
     return response;
   }
 }
