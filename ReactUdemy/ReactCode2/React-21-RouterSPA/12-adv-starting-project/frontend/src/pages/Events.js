1cb55246ce6bf496b6a74d9273634f6c97b399a2
dnjsxoghd <feelchok1234@gmail.com>
2024-02-09 16:01:57 +0900

    Router Action

--- ReactUdemy/ReactCode2/React-21-RouterSPA/12-adv-starting-project/frontend/src/pages/Events.js
+++ ReactUdemy/ReactCode2/React-21-RouterSPA/12-adv-starting-project/frontend/src/pages/Events.js
@@ -1,6 +1,6 @@
-import { useLoaderData, json } from 'react-router-dom';
+import { useLoaderData, json } from "react-router-dom";
 
-import EventsList from '../components/EventsList';
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
@@ -15,16 +15,19 @@ function EventsPage() {
 
 export default EventsPage;
 
-export async function loader() {
-  const response = await fetch('http://localhost:8080/events');
+export async function loader({reqeust, params}) {
+  const response = await fetch("http://localhost:8080/events");
+
+  console.log(reqeust)
+  console.log(params)
 
   if (!response.ok) {
-    // return { isError: true, message: 'Could not fetch events.' };
+    // throw { isError: true, message: 'Could not fetch events.' };
     // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
     //   status: 500,
     // });
     throw json(
-      { message: 'Could not fetch events.' },
+      { message: "Could not fetch events." },
       {
         status: 500,
       }
