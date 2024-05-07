d46ec3072bcf3589b84c48428d61021c7d3f122d
dnjsxoghd <feelchok1234@gmail.com>
2024-05-07 23:48:48 +0900

    useRef-useImperativeHandle-forwardRef-Portals

---
+++ React-Portfolio/Hooks/Hooks-Test-Project/components/Player.jsx
@@ -0,0 +1,43 @@
+import { useRef } from "react";
+import { useState } from "react";
+import { useReducer } from "react";
+
+function reducerFunction(state, action) {
+  if (action.type === "save") {
+    let value = action.value;
+
+    return value;
+  }
+}
+
+export default function Player() {
+  const [initialState, dispatch] = useReducer(reducerFunction, "");
+
+  const [bol, setBol] = useState(false);
+
+  const inputRef = useRef();
+
+  function saveHandler() {
+    dispatch({ type: "save", value: inputRef.current.value});
+  }
+
+  function saveTitleHandler() {
+    setBol(!bol);
+  }
+  console.log(inputRef.current)
+
+  return (
+    <section id="player">
+      <h2>Welcome {bol ? initialState : "Unkown"}</h2>
+      <p>
+        <input
+          ref={inputRef}
+          type="text"
+          onChange={saveHandler}
+          value={initialState}
+        />
+        <button onClick={saveTitleHandler}>Set Name</button>
+      </p>
+    </section>
+  );
+}
--- React-Portfolio/Hooks/Hooks-Test-Project/src/App.jsx
+++ React-Portfolio/Hooks/Hooks-Test-Project/src/App.jsx
@@ -1,72 +1,16 @@
-import { useState, useReducer } from "react";
 import "./App.css";
-import RiverPool from "../components/RiverPool";
-
-function reducerFunc(state, action){
-  if(action.type === 'increment'){
-    return {
-      ...state,
-      counter : state.counter+1
-    }
-  }
-  if(action.type === 'decrement'){
-    return {
-      ...state,
-      counter : state.counter-1
-    }
-  }
-  if(action.type === 'toggle'){
-    return {
-      ...state,
-      showCounter : !state.showCounter
-    }
-  }
-}
+import RiverPool from "../components/useReducerComponents/RiverPool";
+import Player from "../components/Player";
+import ScreenStart from "../components/dialog/ScreenStart";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const [initialState, dispatch] = useReducer(reducerFunc,{
-    counter : 0,
-    showCounter : true
-  });
-
-  function plusTwoHandler(){
-    dispatch({type : 'increment'});
-  }
-
-  function minusTwoHandler(){
-    dispatch({type : 'decrement'});
-  }
-
-  function toggleHandler(){
-    dispatch({type : 'toggle'});
-  }
-
-  function plusButton(){
-    setCount((prevState)=>{
-      return prevState+1;
-    })
-  }
-
-  function minusButton(){
-    setCount((prevState)=>{
-      return prevState-1;
-    })
-  }
-
   return (
     <>
-      <div>{count}</div>
-      <button onClick={plusButton}>++++</button>
-      <button onClick={minusButton}>----</button>
-      <h1>Hello world</h1>
-      {initialState.showCounter ? <div>{initialState.counter}</div> : undefined}
-      <button onClick={plusTwoHandler}>+++</button>
-      <button onClick={minusTwoHandler}>---</button>
-      <button onClick={toggleHandler}>Toggle</button>
+      <Player />
+      <div id="challenges"></div>
+      <RiverPool></RiverPool>
       <div>
-        <RiverPool></RiverPool>
+        <ScreenStart></ScreenStart>
       </div>
     </>
   );
