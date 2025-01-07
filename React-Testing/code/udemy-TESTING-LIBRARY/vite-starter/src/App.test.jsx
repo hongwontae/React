afaf34f5528f702ba7e80fe1d79e00bf9f99036c
dnjsxoghd <feelchok1234@gmail.com>
2025-01-07 23:57:49 +0900

    react-test

---
+++ React-Testing/code/udemy-TESTING-LIBRARY/vite-starter/src/App.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("App contains correct heading", () => {
+  render(<App />);
+  const headingElement = screen.getByRole('heading', {name : /learn React/i });
+  expect(headingElement).toBeInTheDocument();
+});
