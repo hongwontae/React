4ffb075d15aad33a1a6e22cfb995e0f27253e3ce
dnjsxoghd <feelchok1234@gmail.com>
2025-01-09 17:48:59 +0900

    matcher describe fireEvent ARIA logRoles...

--- React-Testing/code/udemy-TESTING-LIBRARY/vite-starter/src/App.jsx
+++ React-Testing/code/udemy-TESTING-LIBRARY/vite-starter/src/App.jsx
@@ -1,7 +1,41 @@
+import "./App.css";
+import { useState } from "react";
+
 function App() {
+  const [color, setColor] = useState("red");
+  const [checkbox, setCheckbox] = useState(false);
+
+  const buttonText = color === "red" ? "blue" : "red";
+
+  function buttonColorHandler() {
+    setColor(buttonText);
+  }
+
+  function checkboxHandler() {
+    setCheckbox(!checkbox);
+  }
+
   return (
     <div>
-      <h1>I'm gonna learn React Testing Library</h1>
+      <button
+        role="button"
+        className={checkbox ? 'gray' : color}
+        onClick={buttonColorHandler}
+        disabled={checkbox ? true : false}
+      >
+        Change {buttonText}!
+      </button>
+      <div>
+        <label htmlFor="disable-button-checkbox" aria-label="dbc">
+          Check
+        </label>
+        <input
+          id="disable-button-checkbox"
+          type="checkbox"
+          onClick={checkboxHandler}
+          value={checkbox}
+        ></input>
+      </div>
     </div>
   );
 }
--- React-Testing/code/udemy-TESTING-LIBRARY/vite-starter/src/App.test.jsx
+++ React-Testing/code/udemy-TESTING-LIBRARY/vite-starter/src/App.test.jsx
@@ -1,8 +1,40 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { getByRole, logRoles } from "@testing-library/dom";
 import App from "./App";
 
-test("App contains correct heading", () => {
-  render(<App />);
-  const headingElement = screen.getByRole('heading', {name : /learn React/i });
-  expect(headingElement).toBeInTheDocument();
-});
+test('test button checked box interaction', ()=>{
+  render(<App/>)
+  const buttonElement = screen.getByRole('button', {name : /blue/i})
+  const checkboxElement = screen.getByRole('checkbox', {name : /dbc/i})
+  expect(buttonElement).toBeEnabled()
+  expect(checkboxElement).not.toBeChecked()
+
+  fireEvent.click(checkboxElement)
+  expect(buttonElement).toBeDisabled()
+
+  fireEvent.click(checkboxElement)
+  expect(buttonElement).toBeEnabled()
+
+
+
+})
+
+test('button color test', ()=>{
+  render(<App/>)
+  const buttonElement = screen.getByRole('button')
+  expect(buttonElement).toHaveClass('red')
+
+  fireEvent.click(buttonElement)
+  expect(buttonElement).toHaveClass('blue')
+
+  const checkbox = screen.getByRole('checkbox', {name : /dbc/i})
+  fireEvent.click(checkbox)
+  expect(buttonElement).toBeDisabled()
+  expect(buttonElement).toHaveClass('gray')
+})
+
+describe('', () => {
+  test();
+  test();
+  test();
+})
